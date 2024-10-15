f90d53cc6bfb04606bc1ace250f9084d78ae479c
Clo91eaf <Clo91eaf@qq.com>
2024-10-15 20:44:18 +0800

    [rtl] refactor consts in datapath

--- hia/src/Datapath.scala
+++ hia/src/Datapath.scala
@@ -50,26 +50,9 @@ class Datapath(val parameter: DatapathParameter)
   override protected def implicitReset: Reset = io.reset
 
   val xlen = parameter.xlen
-  val PC_START = parameter.PC_START
-  val PC_EPC = parameter.ctrl.PC_EPC
-  val PC_ALU = parameter.ctrl.PC_ALU
-  val PC_0 = parameter.ctrl.PC_0
-  val WB_ALU = parameter.ctrl.WB_ALU
-  val WB_MEM = parameter.ctrl.WB_MEM
-  val WB_PC4 = parameter.ctrl.WB_PC4
-  val WB_CSR = parameter.ctrl.WB_CSR
-  val ST_SW = parameter.ctrl.ST_SW
-  val ST_SH = parameter.ctrl.ST_SH
-  val ST_SB = parameter.ctrl.ST_SB
-  val LD_LB = parameter.ctrl.LD_LB
-  val LD_LH = parameter.ctrl.LD_LH
-  val LD_LHU = parameter.ctrl.LD_LHU
-  val LD_LBU = parameter.ctrl.LD_LBU
-  val A_RS1 = parameter.ctrl.A_RS1
-  val B_RS2 = parameter.ctrl.B_RS2
-  val IMM_Z = parameter.ctrl.IMM_Z
+  val ctrl = parameter.ctrl
 
-  val regFile = Module(new RegFile(parameter.xlen))
+  val regFile = Module(new RegFile(xlen))
   val csr = Instantiate(new CSR(parameter.csrParameter))
   val alu = Instantiate(new ALU(parameter.aluParameter))
   val brCond = Instantiate(new BrCond(parameter.brCondParameter))
@@ -101,16 +84,16 @@ class Datapath(val parameter: DatapathParameter)
     */
   val started = RegNext(io.reset.asBool)
   val stall = !io.icache.valid || !io.dcache.valid
-  val pc = RegInit((PC_START - 4).U(xlen.W))
+  val pc = RegInit((parameter.PC_START - 4).U(xlen.W))
   // Next Program Counter
   val next_pc = MuxCase(
     pc + 4.U,
     Seq(
       stall -> pc,
       csr.io.expt -> csr.io.evec,
-      (io.ctrl.pc_sel === PC_EPC) -> csr.io.epc,
-      ((io.ctrl.pc_sel === PC_ALU) || (brCond.io.taken)) -> (alu.io.sum >> 1.U << 1.U),
-      (io.ctrl.pc_sel === PC_0) -> pc
+      (io.ctrl.pc_sel === ctrl.PC_EPC) -> csr.io.epc,
+      ((io.ctrl.pc_sel === ctrl.PC_ALU) || (brCond.io.taken)) -> (alu.io.sum >> 1.U << 1.U),
+      (io.ctrl.pc_sel === ctrl.PC_0) -> pc
     )
   )
   val inst =
@@ -144,12 +127,12 @@ class Datapath(val parameter: DatapathParameter)
   val wb_rd_addr = ew_reg.inst(11, 7)
   val rs1hazard = wb_en && rs1_addr.orR && (rs1_addr === wb_rd_addr)
   val rs2hazard = wb_en && rs2_addr.orR && (rs2_addr === wb_rd_addr)
-  val rs1 = Mux(wb_sel === WB_ALU && rs1hazard, ew_reg.alu, regFile.io.rdata1)
-  val rs2 = Mux(wb_sel === WB_ALU && rs2hazard, ew_reg.alu, regFile.io.rdata2)
+  val rs1 = Mux(wb_sel === ctrl.WB_ALU && rs1hazard, ew_reg.alu, regFile.io.rdata1)
+  val rs2 = Mux(wb_sel === ctrl.WB_ALU && rs2hazard, ew_reg.alu, regFile.io.rdata2)
 
   // ALU operations
-  alu.io.A := Mux(io.ctrl.A_sel === A_RS1, rs1, fe_reg.pc)
-  alu.io.B := Mux(io.ctrl.B_sel === B_RS2, rs2, imm_out)
+  alu.io.A := Mux(io.ctrl.A_sel === ctrl.A_RS1, rs1, fe_reg.pc)
+  alu.io.B := Mux(io.ctrl.B_sel === ctrl.B_RS2, rs2, imm_out)
   alu.io.alu_op := io.ctrl.alu_op
 
   // Branch condition calc
@@ -165,7 +148,11 @@ class Datapath(val parameter: DatapathParameter)
   io.dcache.wdata := rs2 << woffset
   io.dcache.wen := !stall && io.ctrl.st_type.orR
   io.dcache.mask := MuxLookup(Mux(stall, st_type, io.ctrl.st_type), "b0000".U)(
-    Seq(ST_SW -> "b1111".U, ST_SH -> ("b11".U << alu.io.sum(1, 0)), ST_SB -> ("b1".U << alu.io.sum(1, 0)))
+    Seq(
+      ctrl.ST_SW -> "b1111".U,
+      ctrl.ST_SH -> ("b11".U << alu.io.sum(1, 0)),
+      ctrl.ST_SB -> ("b1".U << alu.io.sum(1, 0))
+    )
   )
 
   // Pipelining
@@ -180,14 +167,14 @@ class Datapath(val parameter: DatapathParameter)
     ew_reg.pc := fe_reg.pc
     ew_reg.inst := fe_reg.inst
     ew_reg.alu := alu.io.out
-    ew_reg.csr_in := Mux(io.ctrl.imm_sel === IMM_Z, imm_out, rs1)
+    ew_reg.csr_in := Mux(io.ctrl.imm_sel === ctrl.IMM_Z, imm_out, rs1)
     st_type := io.ctrl.st_type
     ld_type := io.ctrl.ld_type
     wb_sel := io.ctrl.wb_sel
     wb_en := io.ctrl.wb_en
     csr_cmd := io.ctrl.csr_cmd
     illegal := io.ctrl.illegal
-    pc_check := io.ctrl.pc_sel === PC_ALU
+    pc_check := io.ctrl.pc_sel === ctrl.PC_ALU
   }
 
   // Load
@@ -195,10 +182,10 @@ class Datapath(val parameter: DatapathParameter)
   val lshift = io.dcache.data >> loffset
   val load = MuxLookup(ld_type, io.dcache.data.zext)(
     Seq(
-      LD_LH -> lshift(15, 0).asSInt,
-      LD_LB -> lshift(7, 0).asSInt,
-      LD_LHU -> lshift(15, 0).zext,
-      LD_LBU -> lshift(7, 0).zext
+      ctrl.LD_LH -> lshift(15, 0).asSInt,
+      ctrl.LD_LB -> lshift(7, 0).asSInt,
+      ctrl.LD_LHU -> lshift(15, 0).zext,
+      ctrl.LD_LBU -> lshift(7, 0).zext
     )
   )
 
@@ -215,10 +202,13 @@ class Datapath(val parameter: DatapathParameter)
   csr.io.st_type := st_type
 
   // Regfile Write
-  val regWrite =
-    MuxLookup(wb_sel, ew_reg.alu.zext)(
-      Seq(WB_MEM -> load, WB_PC4 -> (ew_reg.pc + 4.U).zext, WB_CSR -> csr.io.out.zext)
-    ).asUInt
+  val regWrite = MuxLookup(wb_sel, ew_reg.alu.zext)(
+    Seq(
+      ctrl.WB_MEM -> load,
+      ctrl.WB_PC4 -> (ew_reg.pc + 4.U).zext,
+      ctrl.WB_CSR -> csr.io.out.zext
+    )
+  ).asUInt
 
   regFile.io.wen := wb_en && !stall && !csr.io.expt
   regFile.io.waddr := wb_rd_addr
